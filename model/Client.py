0720b6941e3029fe78ad96cac718c11bbe72e5ef
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-06-13 13:02:59 -0500

    Getters added to Client.py

--- model/Client.py
+++ model/Client.py
@@ -2,3 +2,9 @@ class Client:
     def __init__(self, name:str, cc:str) -> None:
         self.name_ = name
         self.cc_ = cc
+    
+    def get_name(self):
+        return self.name_
+    
+    def get_cc(self):
+        return self.cc_
