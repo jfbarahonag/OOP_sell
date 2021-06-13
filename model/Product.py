1482b0901c64d73addbeaa0dc1e046f98db2a614
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-06-13 13:02:26 -0500

    Getters added to Product.py

--- model/Product.py
+++ model/Product.py
@@ -3,3 +3,12 @@ class Product:
         self.code_ = code
         self.name_ = name
         self.price_ = price
+
+    def get_code(self):
+        return self.code_
+
+    def get_name(self):
+        return self.name_
+
+    def get_price(self):
+        return self.price_
